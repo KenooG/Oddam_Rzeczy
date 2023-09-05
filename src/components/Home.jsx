ef5522fb31ece4377bbb5459f4b0cc7ad811ea06
KenooG <seba123.2003@wp.pl>
2023-09-05 17:58:46 +0200

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,8 +15,15 @@ const Home = () =>{
                     <div className="navbar"><div className="menu"><Menu/></div>  </div>
 
                   <div className="front">
-
-
+                      <div className="help"><p className="helptxt">Zacznij pomagać! <br/> Oddaj niechciane rzeczy w zaufane ręce</p></div>
+                      <div className="jpg"></div>
+                      <div className="btns">
+                      <a href="">
+                          <div className="btn"><p className="btntxt">ODDAJ <br/> RZECZY</p></div>
+                      </a><a href="">
+                      <div className="btn"><p  className="btntxt"> ZORGANIZUJ <br/> ZBIÓRKĘ</p></div>
+                  </a>
+                      </div>
 
                   </div>
 
