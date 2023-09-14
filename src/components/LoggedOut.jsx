a27d36b8fd3d7658b4f836b822ac5fbb96be56ee
KenooG <seba123.2003@wp.pl>
2023-09-14 17:18:54 +0200

    działający logout

--- src/components/LoggedOut.jsx
+++ src/components/LoggedOut.jsx
@@ -1,4 +1,4 @@
-
+import MenuLoggedOut from "./MenuLoggedOut.jsx";
 
 
 const LoggedOut = () => {
@@ -6,11 +6,34 @@ const LoggedOut = () => {
 
     return(
         <>
+   <div className="loggedoutmain">
+       <div className="upmain">
+           <MenuLoggedOut/>
+           
+           
+       </div>
+       
+       <div className="downmain">
+           <p className="title">Wylogowanie nastąpiło pomyślnie!</p>
+           <div className="dec"></div>
+           <a href="/">
+               <div className="btn">Strona główna</div>
+           </a>
+           
+           
+           
+           
+       </div>
+       
+      
 
-        WYLOGOWANO
 
 
 
+
+
+
+   </div>
         </>
 
 
---
+++ src/components/MenuLoggedOut.jsx
@@ -0,0 +1,58 @@
+import {Link} from "react-scroll";
+
+const MenuLoggedOut = () => {
+
+
+    return (
+        <>
+            <div className="menubox">
+                <div className="up">
+                    <div className="upleft">
+                        <a href="/login">
+                            <div className="upbtn"><p className="btntxt">Zaloguj</p></div>
+                        </a>
+                        <a href="/register">
+                            <div className="upbtn"><p className="btntxt">Załóż konto</p></div>
+                        </a>
+                    </div>
+
+                </div>
+                <div className="down">
+                    <a href="/">
+                        <div className="downbtn">
+                            <p className="btntxt">Start</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div className="downbtn">
+                            <p className="btntxt">O co chodzi?</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div className="downbtn">
+                            <p className="btntxt">O nas</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div className="downbtn">
+                            <p className="btntxt">Fundacja i organizacje</p>
+                        </div>
+                    </a>
+                    <a href="">
+                        <div className="downbtn">
+                            <p className="btntxt">Kontakt</p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+
+        </>
+
+
+    )
+
+
+}
+
+export default MenuLoggedOut
